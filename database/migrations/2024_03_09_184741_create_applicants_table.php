788b84f283d0b4019ed0c05b6ed80be04ed5a918
Jwan <jwanaalfatla1999@gmail.com>
2024-06-15 14:20:26 +0300

    main

--- database/migrations/2024_03_09_184741_create_applicants_table.php
+++ database/migrations/2024_03_09_184741_create_applicants_table.php
@@ -14,17 +14,17 @@ return new class extends Migration {
         Schema::create('applicants', function (Blueprint $table) {
             $table->id();
             $table->string('image')->nullable();
-            $table->json('speciality')->default('{}');
-            $table->json('education')->default('[]');
-            $table->json('languages')->default('[]');
-            $table->json('skills')->default('[]');
-            $table->json('tools')->default('[]');
-            $table->json('details')->default('[]');
-            $table->text('summary')->default('');
-            $table->json('courses')->default('[]');
+            $table->json('speciality');
+            $table->json('education');
+            $table->json('languages');
+            $table->json('skills');
+            $table->json('tools');
+            $table->json('details');
+            $table->text('summary');
+            $table->json('courses');
             $table->json('contact');
-            $table->json('employment')->default('[]');
-            $table->json('activities')->default('[]');
+            $table->json('employment');
+            $table->json('activities');
             $table->boolean('published')->default(false);
 
             $table->unsignedBigInteger('user_id');
