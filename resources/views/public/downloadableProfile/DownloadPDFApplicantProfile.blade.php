5331ef005a4d7eb0e7c5fc3b7f3ef886190d4e61
Jwan <jwanaalfatla1999@gmail.com>
2024-08-04 16:53:02 +0300

    pdf testing

--- resources/views/public/downloadableProfile/DownloadPDFApplicantProfile.blade.php
+++ resources/views/public/downloadableProfile/DownloadPDFApplicantProfile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $applicant->details['fullName'] ?? 'Applicant' }} - Resume</title>
+    <title>{{ $applicant->contact['fullName'] ?? 'Applicant' }} - Resume</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         body {
