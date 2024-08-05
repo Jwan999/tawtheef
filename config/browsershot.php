b61b4e64f7d3a14669f84eea956e6fd2c914473d
Jwan <jwanaalfatla1999@gmail.com>
2024-08-05 09:15:13 +0300

    pdf testing

--- config/browsershot.php
+++ config/browsershot.php
@@ -1,12 +1,7 @@
 <?php
 
-function getValidPath($command, $default) {
-    $path = trim(shell_exec($command));
-    return (file_exists($path) && is_executable($path)) ? $path : $default;
-}
-
 return [
-    'chrome_path' => env('CHROME_PATH', getValidPath('which chromium-browser || which chromium || which google-chrome', '/usr/bin/chromium-browser')),
+    'chrome_path' => env('CHROME_PATH', '/snap/bin/chromium'),
     'node_path' => env('NODE_PATH', getValidPath('which node', '/usr/bin/node')),
     'npm_path' => env('NPM_PATH', getValidPath('which npm', '/usr/bin/npm')),
 ];
