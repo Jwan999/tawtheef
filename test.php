63c270aac9c2eaafd6f285429603b10c9404d84a
Jwan <jwanaalfatla1999@gmail.com>
2024-08-05 09:18:05 +0300

    pdf testing

--- test.php
+++ test.php
@@ -3,5 +3,14 @@ require __DIR__ . '/vendor/autoload.php';
 use Spatie\Browsershot\Browsershot;
 
 $html = '<h1>Test</h1>';
-$pdf = Browsershot::html($html)->noSandbox()->pdf();
+$pdf = Browsershot::html($html)
+    ->setChromePath('/snap/bin/chromium')
+    ->noSandbox()
+    ->setNodeBinary('/usr/bin/node')
+    ->setNpmBinary('/usr/bin/npm')
+    ->setOption('args', ['--no-sandbox', '--disable-setuid-sandbox'])
+    ->setOption('env', ['PUPPETEER_CACHE_DIR' => '/var/www/.cache/puppeteer'])
+    ->pdf();
+
 file_put_contents('/var/www/tawtheef/storage/app/test.pdf', $pdf);
+echo "PDF generated successfully.\n";
