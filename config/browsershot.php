83d6e45e4fca09ff1c178537d080a0630830492a
Jwan <jwanaalfatla1999@gmail.com>
2024-08-05 09:16:41 +0300

    pdf testing

--- config/browsershot.php
+++ config/browsershot.php
@@ -2,6 +2,6 @@
 
 return [
     'chrome_path' => env('CHROME_PATH', '/snap/bin/chromium'),
-    'node_path' => env('NODE_PATH', getValidPath('which node', '/usr/bin/node')),
-    'npm_path' => env('NPM_PATH', getValidPath('which npm', '/usr/bin/npm')),
+    'node_path' => env('NODE_PATH', '/usr/bin/node'),
+    'npm_path' => env('NPM_PATH', '/usr/bin/npm'),
 ];
