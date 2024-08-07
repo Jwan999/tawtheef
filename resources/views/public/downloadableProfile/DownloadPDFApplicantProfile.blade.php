9135f54f2503bc4cc07cd8a9456824d916a21439
Jwan <jwanaalfatla1999@gmail.com>
2024-08-07 14:36:43 +0300

    main

--- resources/views/public/downloadableProfile/DownloadPDFApplicantProfile.blade.php
+++ resources/views/public/downloadableProfile/DownloadPDFApplicantProfile.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-{{--    <title>{{ $applicant->contact['fullName'] ?? 'Applicant' }} - Resume</title>--}}
+    <title>{{ $applicant->contact['fullName'] ?? 'Applicant' }} - Resume</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
     <style>
         body {
             font-family: 'Dosis', sans-serif;
