72d7bc247471b57afa87a635feb060c6ee0858e7
Jwan <jwanaalfatla1999@gmail.com>
2024-08-13 11:27:27 +0300

    vite fixes

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,9 @@ export default defineConfig({
     server: {
         port: 5173,
         open: true,
+        watch: {
+            ignored: ['**/vendor/**', '**/node_modules/**', '**/storage/**'],
+        },
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:8000',
