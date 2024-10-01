6d6109ed1c9ec324527a4817280bc94605efe125
Jwan <jwanaalfatla1999@gmail.com>
2024-10-01 04:11:59 +0300

    main

--- resources/views/public/mainViewComponents/PreviewAllComponent.vue
+++ resources/views/public/mainViewComponents/PreviewAllComponent.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <div class="md:w-3/12 w-full mt-16">
-            <h1 class="text-start py-1 my-2 px-3 font-bold tracking-wider text-2xl md:text-3xl uppercase">
+            <h1 class="text-start py-1 my-2 px-3 font-bold tracking-wider text-2xl md:text-3xl">
                 Search Results
             </h1>
             <div class="w-10/12 mt-6">
-                <div class="-mt-4 h-2.5 mb-2 w-11/12 rounded-br-2xl bg-orange"></div>
-                <div class="-mt-3 ml-8 h-2.5 w-full rounded-br-2xl bg-dark"></div>
+                <div class="-mt-4 h-2.5 mb-2 w-full rounded-full shadow-custom-3d bg-orange"></div>
+
             </div>
         </div>
 
